c11cb3d101b78b8054e17e51897bd312608821e7
KarlLuther <ivanparapanov1337@gmail.com>
2023-04-17 22:36:33 +0100

    saved bacis structure for main

--- src/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export const fetchItems = () => {
-  return axios
-    .get(`https://nc-games-gry8.onrender.com/api/reviews`)
-    .then((response) => {
-      return response.data.reviews;
-    });
-};
--- src/components/ReviewSearchPage.jsx
+++ src/components/ReviewSearchPage.jsx
@@ -2,50 +2,7 @@ import { useEffect, useState } from "react";
 import { fetchItems } from "../api";
 
 const ReviewSearchPage = () => {
-  const [listItems, setListItems] = useState([]);
-  const [selectedCategory, setSelectedCategory] = useState("");
-  useEffect(() => {
-    fetchItems(selectedCategory).then((items) => {
-      setListItems(items);
-    });
-  }, [selectedCategory]);
-  return (
-    <section>
-      <h2>Review Search Page</h2>
-      <ul>
-        {listItems.map((review) => {
-          const { title, owner, review_img_url, category, votes, review_id } =
-            review;
-
-          return (
-            <li className="item-to-sell" key={review_id}>
-              <h3>{title}</h3>
-              <img
-                src={review_img_url}
-                alt={` ${title}`}
-                className="review-img"
-              />
-              <p>Author: {owner}</p>
-              <p>Category: {category}</p>
-              <p>Votes: {votes}</p>
-            </li>
-          );
-        })}
-      </ul>
-    </section>
-  );
+  return <p>Review Search</p>;
 };
 
 export default ReviewSearchPage;
-
-// {
-//   title: 'Agricola',
-//   designer: 'Uwe Rosenberg',
-//   owner: 'mallionaire',
-//   review_img_url:
-//     'https://images.pexels.com/photos/974314/pexels-photo-974314.jpeg?w=700&h=700',
-//   review_body: 'Farmyard fun!',
-//   category: 'euro game',
-//   created_at: new Date(1610964020514),
-//   votes: 1
-// },
